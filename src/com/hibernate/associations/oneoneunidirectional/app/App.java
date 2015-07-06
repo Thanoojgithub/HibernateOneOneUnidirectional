9727bc2c2d0a0178fde818dc6d5a556e2bdb8e89
javaeecub@gmail.com <javaeecub@gmail.com>
2015-07-06 21:56:15 +0530

    c3p0 configuration
    c3p0 configuration

---
+++ src/com/hibernate/associations/oneoneunidirectional/app/App.java
@@ -0,0 +1,28 @@
+package com.hibernate.associations.oneoneunidirectional.app;
+
+import java.util.Calendar;
+
+import com.hibernate.associations.oneoneunidirectional.pojo.Employee;
+import com.hibernate.associations.oneoneunidirectional.pojo.EmployeeDetails;
+import com.hibernate.associations.oneoneunidirectional.util.HibernateUtil;
+
+import org.hibernate.Session;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hibernate one to one (Annotation)");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Employee employee = new Employee("hanuma");
+		EmployeeDetails employeeDetails = new EmployeeDetails();
+		employeeDetails.setLocation("kishkinda");
+		employeeDetails.setDoj(Calendar.getInstance().getTime());
+		employeeDetails.setEmployee(employee);
+		session.save(employeeDetails);
+		session.getTransaction().commit();
+		session.close();
+		System.out.println("Done");
+	}
+
+}
